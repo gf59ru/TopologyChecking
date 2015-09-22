1a4a07a21ac0ce5a21f077a31f2c07490e34c28f
General Failure <gf59ru@live.com>
2015-09-22 09:22:05 +0500

    some log

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
 
   def index
     unless current_user.nil?
+      puts current_user.id.class.name
       @operation_types = OperationType.all
       @operations = current_user.operations
     end
