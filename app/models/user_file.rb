5b785a7cbfa7a79f4c0725d12c82338d8bf1d0f7
General Failure <gf59ru@live.com>
2015-10-02 15:39:22 +0500

    Disk file deletes now with user_file table row

--- app/models/user_file.rb
+++ app/models/user_file.rb
@@ -3,6 +3,7 @@ class UserFile < ActiveRecord::Base
   FILE_TYPE_REQUISITES = 0
 
   belongs_to :user
+  after_destroy :delete_file
 
   def humanized_name
     name = File.basename file_path
@@ -15,6 +16,12 @@ class UserFile < ActiveRecord::Base
 
   def humanized_name_with_creation_date
     "#{humanized_name} (#{I18n.l created_at})"
+  end
+
+  private
+
+  def delete_file
+    File.delete file_path if File.exist? file_path
   end
 
 end
